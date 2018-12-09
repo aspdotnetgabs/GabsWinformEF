47e20f9af973ee63d0aeeb0696e5732f263f37a3
Gabs <Gabs@DESKTOP-14Q8T2V>
2018-12-10 02:31:14 +0800

    init commit

---
+++ GabsWinformEF/Form1.cs
@@ -0,0 +1,44 @@
+﻿using GabsWinformEF.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GabsWinformEF
+{
+    public partial class Form1 : Form
+    {
+        MyDbContext _db = new MyDbContext();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var contact = new Contact
+            {
+                Name = txtName.Text,
+                Phone = txtPhone.Text,
+                Email = txtEmail.Text
+            };
+
+            _db.Contacts.Add(contact);
+            _db.SaveChanges();
+
+            dataGridContacts.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            contactBindingSource.DataSource = _db.Contacts.ToList();
+        }
+
+    }
+}
---
+++ GabsWinformEF/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GabsWinformEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabsWinformEF
+{
+    class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DbConnDbJetAccess")
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
