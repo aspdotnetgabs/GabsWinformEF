b23d70bbb29f8dd12055eeeaa22cdd95eec58bf5
hubert17 <hewbertgabon@gmail.com>
2019-02-24 05:19:59 +0800

    fix conn string

--- GabsWinformEF/MyDbContext.cs
+++ GabsWinformEF/MyDbContext.cs
@@ -10,7 +10,7 @@ namespace GabsWinformEF
 {
     class MyDbContext : DbContext
     {
-        public MyDbContext() : base("DbConnSqlServerSomee")
+        public MyDbContext() : base("DbConnSqlServerRemote")
         {
 
         }
