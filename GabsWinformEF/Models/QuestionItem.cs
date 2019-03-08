8fc5b382bbe01988b17f335cde1230b241e1425a
hubert17 <hewbertgabon@gmail.com>
2019-03-09 06:45:34 +0800

    for Gracelle

---
+++ GabsWinformEF/Models/QuestionItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabsWinformEF.Models
+{
+    class QuestionItem
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string CorrectAnsLetter { get; set; }
+        public string ChoiceA { get; set; }
+        public string ChoiceB { get; set; }
+        public string ChoiceC { get; set; }
+        public string ChoiceD { get; set; }
+    }
+}
