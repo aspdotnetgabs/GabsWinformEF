42450d23d62aa2c4262d438d5d2b71199ac43ee2
hubert17 <hewbertgabon@gmail.com>
2019-03-10 03:03:30 +0800

    some changes 2

--- GabsWinformEF/Models/User.cs
+++ GabsWinformEF/Models/User.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
-class User
+public partial class User
 {
     public int Id { get; set; }
     //Login info
